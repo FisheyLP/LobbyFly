eff2cb63442bd520dca075ea4cf3a3f28d7a55db
Null Dev <NL-Dev@yandex.com>
2017-01-17 17:55:50 +0100

    THIS CONTAINS ERRORS

--- src/org/nulldev/LobbyFly/Main.java
+++ src/org/nulldev/LobbyFly/Main.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -32,6 +31,7 @@ public class Main extends JavaPlugin implements Listener {
         return instance;
     }
     
+    @Override
     public void onEnable() {
     	this.checkConfig();
         instance = this;
@@ -39,7 +39,7 @@ public class Main extends JavaPlugin implements Listener {
         PluginDescriptionFile pdfFile = this.getDescription();
         this.logger.info("[" + String.valueOf(String.valueOf(pdfFile.getName())) + "] Version: " + 
         pdfFile.getVersion() + " by NullDev for EpticMC has been enabled!");
-        this.getCommand("lfly").setExecutor((CommandExecutor)new CmdHandle(this));
+        this.getCommand("lfly").setExecutor(new CmdHandle());
     }
     
     //This works for 1.8.X
@@ -123,5 +123,6 @@ public class Main extends JavaPlugin implements Listener {
         PluginDescriptionFile pdfFile = this.getDescription();
         this.logger.info(String.valueOf("[" + String.valueOf(pdfFile.getName())) + "] Version: " + 
         pdfFile.getVersion() + " by NullDev for EpticMC has been disabled!");
+        instance = null;
     }
 }
