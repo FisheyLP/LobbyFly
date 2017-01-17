89f2cf398fc2f59222cdd9dc4e80c709b0ba1af8
FisheyLP <FisheyLP@gmail.com>
2017-01-17 18:07:58 +0100

    Update CmdHandle.java

--- src/org/nulldev/LobbyFly/CmdHandle.java
+++ src/org/nulldev/LobbyFly/CmdHandle.java
@@ -15,12 +15,11 @@ import org.nulldev.LobbyFly.Main;
 public class CmdHandle implements CommandExecutor {
 
 	public ArrayList<String> denylist = new ArrayList<String>();
-	public String prefix;
+	public String prefix = Main.getInstance().getConfig().getString("prefix");;
     
 	@Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] a) {
-    	prefix = Main.getInstance().getConfig().getString("prefix");
-		Player p; //p = (Player)sender;
+    	
 		JSONParser jsonParse = new JSONParser();
         try
         {
@@ -38,37 +37,40 @@ public class CmdHandle implements CommandExecutor {
             e.printStackTrace();
         }
         if (!sender.hasPermission("lobbyfly.admin") || !sender.isOp()) {
-            sender.sendMessage(colWrap(prefix + " &cNo permission!"));
+            sender.sendMessage(prefix + " §cNo permission!");
             return true;
         }
         if (!(sender instanceof Player)) {
-            sender.sendMessage(colWrap(prefix + " &cThis can only be executed ingame!"));
+            sender.sendMessage(prefix + " §cThis can only be executed ingame!");
             return true;
         }
+	    
+	Player p = (Player) sender;
+	    
         if (a.length < 1 || !a[0].equals("deny") && a[0].equals("denylist") && !a[0].equals("undeny") || a[0].equalsIgnoreCase("help")) {
-            sender.sendMessage(colWrap(prefix + " &fHelp Menu:"));
-            sender.sendMessage(colWrap("&b----------&9============== &6&lHelp &9==============&b----------"));
-            sender.sendMessage(colWrap("&b/lfly help               &f&l| &9Display this help menu"));
-            sender.sendMessage(colWrap("&b/lfly deny (username)    &f&l| &9Deny a user from flying"));
-            sender.sendMessage(colWrap("&b/lfly undeny (username)  &f&l| &9Allow a user to fly again"));
-            sender.sendMessage(colWrap("&b/lfly denylist           &f&l| &9List all denied users"));
-            sender.sendMessage(colWrap("&b/lfly status (username)  &f&l| &9See if a user is denied"));
-            sender.sendMessage(colWrap("&b------------------------------------------------------"));
-            sender.sendMessage(colWrap("&6       &l~ &b&lNOTE: &bYou can use &9/lobbyfly &bor &9/lfly ~"));
-            sender.sendMessage(colWrap("This plugin was coded by &a&lNullDev &ffor &c&lEptic&4&lMC&f.com"));
-            sender.sendMessage(colWrap("&b======================================================"));
+            sender.sendMessage(prefix + " §fHelp Menu:");
+            sender.sendMessage("§b----------§9============== §6§lHelp §9==============§b----------");
+            sender.sendMessage("§b/lfly help               §f§l| §9Display this help menu");
+            sender.sendMessage("§b/lfly deny (username)    §f§l| §9Deny a user from flying");
+            sender.sendMessage("§b/lfly undeny (username)  §f§l| §9Allow a user to fly again");
+            sender.sendMessage("§b/lfly denylist           §f§l| §9List all denied users");
+            sender.sendMessage("§b/lfly status (username)  §f§l| §9See if a user is denied");
+            sender.sendMessage("§b------------------------------------------------------");
+            sender.sendMessage("§6       §l~ §b§lNOTE: §bYou can use §9/lobbyfly §bor §9/lfly ~");
+            sender.sendMessage("This plugin was coded by §a§lNullDev §ffor §c§lEptic§4§lMC§f.com");
+            sender.sendMessage("§b======================================================");
             return true;
         }
         if (a[0].equalsIgnoreCase("deny")) {
-            p = (Player)sender;
+           
 
         }
         if (a[0].equalsIgnoreCase("denylist")) {
-            p = (Player)sender;
+         
 
         }
         if (a[0].equalsIgnoreCase("undeny")) {
-            p = (Player)sender;
+          
 
         }
         return true;
